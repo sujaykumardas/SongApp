83f08bae8e3d5d24d82f3afa4982fe6279d32d98
Shivam Jaiswal <sjaiswal@techracers.com>
2017-07-31 17:37:37 +0530

    last

--- songApp/controllers/homepage.js
+++ songApp/controllers/homepage.js
@@ -9,66 +9,48 @@ import {Reply} from '../models/reply';
 export const setHomePage = (req, res ,next) => {
 
   Song.find({}, function(err, songs) {
-    console.log("song print",songs);
+    console.log("homepage",req.user);
     if(err){
       console.log(err);
     }
+    req.i=0;
     req.data=[];
     req.songNames=[];
     req.songId=[];
     req.comments=[];
     req.likes=[];
-    req.reply=[];
+    let reply=[];
+    let i=0;
 
-    Comment.find({}, function(err, comm_arr) {
-      if(err){
-        console.log(err);
-      }
-      else{
-        console.log("comments",comm_arr);
-        if(comm_arr.length>0){
-    //let i=0;
-          comm_arr.forEach(function(u, index)  {
-            console.log(u.comment,index);
-            req.reply[index]={};
-            req.reply[index].key=u.comment;
-            Reply.find({repliedOnComment:u._id}, function(err, reps){
-              if(err){
-                console.log(err);
-              }
-              else{              
-                req.reply[index].value=reps;
-                
-                console.log("rep",index,req.reply);
-               
-                songs.forEach(function(song){             
-             	    req.data.push(song.songPath);
-                  req.songNames.push(song.songName);
-             	    req.songId.push(song._id);
-                  req.likes.push(song.likes.length);             
-                  if(typeof song.comments !== 'undefined'){                
-                    req.comments.push(song.comments);
-                  }
-                });
-                //next();
-                if(comm_arr.length==0){
-                  next();
-                }
-                else if(index+1 === comm_arr.length){
-                  next();
-                }
-                
-              }
-            });
-            //i++;
+    songs.forEach(function(song,sindex){             
+      req.data.push(song.songPath);
+      req.songNames.push(song.songName);
+      req.songId.push(song._id);
+      req.likes.push(song.likes.length);             
+      if(typeof song.comments !== 'undefined'){                
+        req.comments.push(song.comments);        
+        song.comments.forEach(function(com,cindex){
+          //console.log(com.commentId.comment);
+          reply[i]={};
+          reply[i].key=com.commentId.comment;
+          console.log(reply[i].key);
+          Reply.find({repliedOnComment:com.commentId}, function(err, reps) {
+            console.log("i",i);
+            reply[i].value=reps;
+            console.log("reply",reps);
           });
-        }
+          i++;
+        });
+        console.log(song.comments,sindex);
       }
     });
-       
+    //if()
+    console.log("replies",reply);
+      next();
+                
   }).populate('comments.commentId');
 };
 
 export const renderHomepage = (req, res) => {    
-    res.render('pages/homepage',{data:req.data, user:req.user, names:req.songNames, ids:req.songId, comments:req.comments, likes:req.likes, replies:req.reply});
+    res.render('pages/homepage',{data:req.data, user:req.user, names:req.songNames, ids:req.songId, comments:req.comments, likes:req.likes});//, replies:req.reply});
 };
